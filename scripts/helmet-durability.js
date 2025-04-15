83904b66674c7a3f6f4b6cfd35254e0dee3c7472
andrewschetera <andrews.chetera@gmail.com>
2025-04-15 00:01:17 -0300

    Create helmet-durability.js

---
+++ scripts/helmet-durability.js
@@ -0,0 +1,25 @@
+// Adiciona campos de durabilidade aos capacetes
+
+export function registerHelmetDurability() {
+  Hooks.on('renderItemSheet', (app, html, data) => {
+    const item = app.item;
+
+    if(item.isOwner && item.type === "helmet") {      
+      const durability = item.flags.world?.durability ?? '0';
+      const notch = item.flags.world?.notch ?? '0';
+      let myContent = $(`<table style="border-style:none;border-width:0px" border="0"><tbody>
+<tr>
+          <th style="border-width:0px">Durability</th>
+          <th style="border-width:0px">Notches</th>
+</tr><tr>
+          <td style="border-width:0px">
+              <input name="flags.world.durability" type="text" value="${durability}">
+          </td>
+          <td style="border-width:0px">
+              <input name="flags.world.notch" type="text" value="${notch}">
+          </td>
+</tr></tbody></table>`);
+      html.find('input[name="system.quantity"]').parent().parent().after(myContent);
+    }
+  });
+}
