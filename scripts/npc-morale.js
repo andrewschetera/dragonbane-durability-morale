cfef806516d74671e7802e951c853dd17c5025bd
andrewschetera <andrews.chetera@gmail.com>
2025-04-15 00:21:03 -0300

    Update npc-morale.js

--- scripts/npc-morale.js
+++ scripts/npc-morale.js
@@ -6,8 +6,12 @@ export function registerNPCMorale() {
 
     if(game.user.isGM && actor.type == "npc") {      
       const morale = actor.flags.world?.morale ?? '';
+      
+      // Obtenha a string traduzida
+      const moraleLabel = game.i18n.localize("dragonbane-durability-morale.morale.title");
+      
       let myContent = $(`<tr>
-                          <th>Morale</th>
+                          <th>${moraleLabel}</th>
                           <td><input class="attribute-input active-effect-property" data-property="flags.world.morale" name="flags.world.morale" type="text" value="${morale}"></td>
                       </tr>
       `);
